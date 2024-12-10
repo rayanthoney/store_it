49773f81463bdd8bae5b93f66b0c63d1a7ee9acc
RayAnthoney <rmaxey911@gmail.com>
2024-12-10 14:32:01 -0700

    set up (auth) folder for the sign in and sign up page

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>This is the Sign In Page</div>
+  )
+}
+
+export default SignIn;
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="center">
-      <h1 className="text-3xl text-brand">
-        StoreIt - The only storage solution you need!
+    <div className="flex-center h-screen">
+      <h1 className="h1">
+        Tone911 - The only storage solution you need!
       </h1>
     </div>
   );
