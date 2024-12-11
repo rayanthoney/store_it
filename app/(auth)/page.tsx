c1d21838254fd3abfa84eb51b31c11df262933dd
RayAnthoney <rmaxey911@gmail.com>
2024-12-11 16:28:08 -0700

    used the figma design to load public assets and layout.tsx file

--- app/(auth)/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SignUp = () => {
-  return (
-    <div>This is the Sign Up Page</div>
-  )
-}
-
-export default SignUp
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 
 const SignIn = () => {
-  return (
-    <div>This is the Sign In Page</div>
-  )
+  return <div>This is the Sign In Page</div>;
+  
 }
 
 export default SignIn;
