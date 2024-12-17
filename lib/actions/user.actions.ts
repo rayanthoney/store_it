9268d3f171d41867e274d5df64293b066cc92f5f
RayAnthoney <rmaxey911@gmail.com>
2024-12-17 07:03:17 -0700

    Setup account with Appwrite to handle backend and storage and installed node-appwrite

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,47 @@
+"use server";
+
+import { createAdminClient } from "@/lib/appwrite/config";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import clsx from "clsx";
+import { ID, Query } from "node-appwrite";
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    [Query.equal("email", [email])]
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.log(error, message);
+  throw error;
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send email OTP");
+  }
+};
+
+const createAccount = async ({
+  fullname,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+};
