3cd9cc27fe7698a2c14ae0801b193c4300ea56fe
RayAnthoney <rmaxey911@gmail.com>
2025-02-04 18:49:29 -0700

    creating an OTP-Input

--- components/OTPModal.tsx
+++ components/OTPModal.tsx
@@ -16,10 +16,17 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import Image from "next/image";
 
 import React, { useState } from "react";
 
-const OtpModal = () => {
+const OtpModal = ({
+  accountId,
+  email,
+}: {
+  accountId: string;
+  email: string;
+}) => {
   const [isOpen, setIsOpen] = useState(true);
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(true);
@@ -42,32 +49,43 @@ const OtpModal = () => {
   };
 
   return (
-    <AlertDialog>
-      <AlertDialogTrigger>Open</AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+    <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+      <AlertDialogContent className="shad-alert-dialog">
+        <AlertDialogHeader className="relative flex justify-center">
+          <AlertDialogTitle className="h2 text-center">
+            Enter your OTP
+            <Image
+              src={"/assets/icons/close-dark.svg"}
+              alt="close"
+              width={20}
+              height={20}
+              onClick={() => setIsOpen(false)}
+              className="otp-close-button"
+            />
+          </AlertDialogTitle>
+          <AlertDialogDescription className="subtitle-2 text-center text-light-100">
+            We&apos;ve sent a code to your{" "}
+            <span className="pl-1 text-brand">{email}</span>
           </AlertDialogDescription>
         </AlertDialogHeader>
 
-        <InputOTP maxLength={6}>
-          <InputOTPGroup>
-            <InputOTPSlot index={0} />
-            <InputOTPSlot index={1} />
-            <InputOTPSlot index={2} />
-          </InputOTPGroup>
-          <InputOTPSeparator />
-          <InputOTPGroup>
-            <InputOTPSlot index={3} />
-            <InputOTPSlot index={4} />
-            <InputOTPSlot index={5} />
-          </InputOTPGroup>
+        <InputOTP maxLength={6} value={password} onChange={setPassword}>
+          <InputOTPGroup className="shad-otp">
+            <InputOTPSlot index={0} className="shad-otp-slot"/>
+            <InputOTPSlot index={1} className="shad-otp-slot"/>
+            <InputOTPSlot index={2} className="shad-otp-slot"/>          
+            <InputOTPSlot index={3} className="shad-otp-slot"/>
+            <InputOTPSlot index={4} className="shad-otp-slot"/>
+            <InputOTPSlot index={5} className="shad-otp-slot"/>
+          </InputOTPGroup>          
         </InputOTP>
 
         <AlertDialogFooter>
+          <div className="flex w-full flex-col gap-4">
+
+          </div>
+
+
           <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction>Continue</AlertDialogAction>
         </AlertDialogFooter>
